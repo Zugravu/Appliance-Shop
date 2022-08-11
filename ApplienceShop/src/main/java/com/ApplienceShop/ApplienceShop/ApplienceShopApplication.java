59ab2bf32c4dc19c9c9148a58bbdde7e1120bec3
Zugravu Alexandru Stefan <alexandruzugra@gmail.com>
2022-08-11 16:16:36 +0300

    init

---
+++ ApplienceShop/src/main/java/com/ApplienceShop/ApplienceShop/ApplienceShopApplication.java
@@ -0,0 +1,13 @@
+package com.ApplienceShop.ApplienceShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplienceShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplienceShopApplication.class, args);
+	}
+
+}
---
+++ ApplienceShop/src/test/java/com/ApplienceShop/ApplienceShop/ApplienceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApplienceShop.ApplienceShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplienceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
